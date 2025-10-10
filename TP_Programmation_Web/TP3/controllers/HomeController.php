61c2ff86af8a88e49ed10ef3c737877bcfcd17d5
AdiyoFouad <odjfouad38@gmail.com>
2025-10-10 12:34:09 +0200

    PW - TP3 / HomeController

--- TP_Programmation_Web/TP3/controllers/HomeController.php
+++ TP_Programmation_Web/TP3/controllers/HomeController.php
@@ -1,4 +1,16 @@
 <?php
 
+    require_once '../config/config.php';
     require_once("../DAO/ContactDAO.php");
+
+    class HomeController {
+        public function index() {
+            global $pdo;
+            $contactDAO = new ContactDAO($pdo);
+            $contacts = $contactDAO->getContacts();
+            require '../views/home.php';
+        }
+    }
+?>
+
 ?>
