963d07d7224aa616d52573966ba2234927320e1d
AdiyoFouad <odjfouad38@gmail.com>
2025-10-10 13:02:36 +0200

    PW - TP3 / ViewContactController

---
+++ TP_Programmation_Web/TP3/controllers/ViewContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once '../models/ContactModel.php';
+    require_once '../config/config.php';
+    require_once '../DAO/ContactDAO.php';
+    class ViewContactController {
+        public function viewContact() {
+            if (isset($_GET['id'])) {
+                global $pdo;
+                $contactDAO = new ContactDAO($pdo);
+                $contact = $contactDAO->getById($_GET['id']);
+                require '../views/view_contact.php';
+            }
+        }
+
+    }
+    
+
+?>
