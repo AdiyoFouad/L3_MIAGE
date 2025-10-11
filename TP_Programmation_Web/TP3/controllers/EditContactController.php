9d24e114a0f3ac4871ce623637a7aa338c64aa96
AdiyoFouad <odjfouad38@gmail.com>
2025-10-11 20:47:56 +0200

    PW - TP3 / EditContactController

---
+++ TP_Programmation_Web/TP3/controllers/EditContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once '../models/ContactModel.php';
+    require_once '../config/config.php';
+    require_once '../DAO/ContactDAO.php';
+    class EditContactController {
+        public function editContact() {
+            if (isset($_POST['id']) && isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["email"]) && isset($_POST["telephone"])) {
+                $contact = new ContactModel($_POST['id'], $_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["telephone"]);
+                $contact->update();
+                header("Location: index.php");
+            }
+        }
+
+        public function showForm() {
+            // Affichage du formulaire
+            require '../views/edit_contact.php';
+        }
+    }
+    
+
+?>
