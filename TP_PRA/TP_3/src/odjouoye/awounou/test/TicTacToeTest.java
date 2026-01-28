ad5dd844c02b0a1cfa05e9830d0115616e85681c
AdiyoFouad <odjfouad38@gmail.com>
2026-01-28 20:47:55 +0100

    TP PRA / TP3 - Test getWinner()

--- TP_PRA/TP_3/src/odjouoye/awounou/test/TicTacToeTest.java
+++ TP_PRA/TP_3/src/odjouoye/awounou/test/TicTacToeTest.java
@@ -19,17 +19,17 @@ public class TicTacToeTest {
 	@BeforeEach
 	public void setUp() {
 //		morpions = new TicTacToeV1();
-		morpions = new TicTacToeV2();
-//		morpions = new TicTacToeV3();
-//		morpions = new TicTacToeV4();
+//		morpions = new TicTacToeV2(); //x
+//		morpions = new TicTacToeV3(); //x
+//		morpions = new TicTacToeV4(); //x
 //		morpions = new TicTacToeV5();
-//		morpions = new TicTacToeV6();
-//		morpions = new TicTacToeV7();
-//		morpions = new TicTacToeV8();
-//		morpions = new TicTacToeV9();
+//		morpions = new TicTacToeV6(); //x
+//		morpions = new TicTacToeV7(); //x
+//		morpions = new TicTacToeV8(); //x
+//		morpions = new TicTacToeV9(); 
 //		morpions = new TicTacToeV10();
-//		morpions = new TicTacToeV11();
-//		morpions = new TicTacToeV12();
+		morpions = new TicTacToeV11();
+//		morpions = new TicTacToeV12(); //x
 
 	}
 
@@ -52,7 +52,6 @@ public class TicTacToeTest {
 		morpions.play(0, 0);
 		testInvariant();
 		assertTrue(!morpions.legalMove(0, 0), "la case (0,0) est déjà remplie");
-
 	}
 
 	@Test
@@ -73,10 +72,161 @@ public class TicTacToeTest {
 	
 	@Test
 	public void testGetJoueur() {
-		Owner o = morpions.getTurn();
-		assertTrue(o != null, "La méthode getTurn() doit retourner un Owner");
-		//assertTrue(o = )
+		Owner owner = morpions.getTurn();
+
+		assertTrue(owner.equals(Owner.FIRST), "La méthode getTurn() doit retourner un Owner");
+		
+	
+		morpions.play(1, 1);
+		owner = morpions.getTurn();
+		assertTrue(owner.equals(Owner.SECOND), "Le joueur courant après le premier coup ne peut être autre que le joueur SECOND");	
 	}
+	
+	@Test
+	public void testInit() {
+		// Scénario(s) vérifiant l’état du jeu avant le premier coup :
+		// * non-fin de partie
+		// * cases accessibles
+		
+		assertTrue(morpions.getTurn().equals(Owner.FIRST), "Le joueur courant au début de la partie ne peut être autre que le joueur FIRST");
+		
+		boolean caseVide = true;
+		
+		for (int i = 0; i < 3 && caseVide; i++ ) {
+			for (int j = 0; j < 3 && caseVide; j++ ) {
+				if (!morpions.getSquare(i, j).equals(Owner.NONE)) {
+					caseVide = false;
+				}
+			}
+		}
+		assertTrue(caseVide, "Aucun joueur ne devrait encore jouer au début de la partie");
+		
+	}
+	
+	@Test
+	public void testGetVainqueur0() {
+		Owner winner = morpions.getWinner();
+		assertTrue(!(winner.equals(Owner.FIRST) || winner.equals(Owner.SECOND)), "Au début de la partie on ne peut pas avoir de vainqueur");
+		
+		morpions.play(0, 1);
+		assertTrue(!(winner.equals(Owner.FIRST) || winner.equals(Owner.SECOND)), "Après un coup on ne peut pas avoir de vainqueur");	
+		
+	}
+	
+	@Test
+	public void testGetVainqueur1() {
+		//Victoire sur l'horizontal
+		morpions.play(0, 0);
+		morpions.play(1, 1);
+		morpions.play(0, 1);
+		morpions.play(2, 1);
+		morpions.play(0, 2);
+		assertTrue(morpions.getWinner().equals(Owner.FIRST) && morpions.numberOfRounds() == 5 && morpions.gameOver(), "TestGetVainqueur2 : Victoire horizontal (a)");
+		
+		morpions.restart();
+		morpions.play(0, 0);
+		morpions.play(1, 2);
+		morpions.play(2, 1);
+		morpions.play(1, 1);
+		morpions.play(0, 2);
+		morpions.play(2, 2);
+		morpions.play(2, 0);
+		morpions.play(1, 0);
+		assertTrue(morpions.getWinner().equals(Owner.SECOND)&& morpions.numberOfRounds() == 8 && morpions.gameOver(), "TestGetVainqueur2 : Victoire horizontal (b)");
+		
+		morpions.restart();
+		morpions.play(2, 2);
+		morpions.play(0, 2);
+		morpions.play(2, 0);
+		morpions.play(1, 2);
+		morpions.play(2, 1);
+		assertTrue(morpions.getWinner().equals(Owner.FIRST)&& morpions.numberOfRounds() == 5 && morpions.gameOver(), "TestGetVainqueur2 : Victoire horizontal (c)");	
+	}
+	
+	@Test
+	public void testGetVainqueur2() {
+		// Victoire sur la verticale
+		morpions.play(2, 1);
+		morpions.play(0, 0); 
+		morpions.play(1, 1);
+		morpions.play(1, 0);
+		morpions.play(2, 2);
+		morpions.play(2, 0);
+		assertTrue(morpions.getWinner().equals(Owner.SECOND) && morpions.numberOfRounds() == 6 && morpions.gameOver(), "TestGetVainqueur3 : Victoire verticale (a)");
+
+		morpions.restart();
+		morpions.play(0, 1);
+		morpions.play(0, 0);
+		morpions.play(1, 1);
+		morpions.play(2, 2);
+		morpions.play(2, 1);
+		assertTrue(morpions.getWinner().equals(Owner.FIRST) && morpions.numberOfRounds() == 5 && morpions.gameOver(), "TestGetVainqueur3 : Victoire verticale (b)");
+		
+		morpions.restart();
+		morpions.play(0, 0);
+		morpions.play(0, 2);
+		morpions.play(1, 1);
+		morpions.play(1, 2);
+		morpions.play(2, 1);
+		morpions.play(2, 2);
+		assertTrue(morpions.getWinner().equals(Owner.SECOND) && morpions.numberOfRounds() == 6 && morpions.gameOver(), "TestGetVainqueur3 : Victoire verticale (c)");
+	}
+	
+	@Test
+	public void testGetVainqueur3() {
+		//Victoire sur Diagonal
+
+		morpions.play(0, 0);
+		morpions.play(0, 1);
+		morpions.play(1, 1);
+		morpions.play(0, 2);
+		morpions.play(2, 2);
+		assertTrue(morpions.getWinner().equals(Owner.FIRST) && morpions.numberOfRounds() == 5 && morpions.gameOver(), "TestGetVainqueur3 : Victoire diagonale (a)");
+		
+		morpions.restart();
+		morpions.play(1, 2);
+		morpions.play(0, 2);
+		morpions.play(1, 0);
+		morpions.play(1, 1);
+		morpions.play(0, 0);
+		morpions.play(2, 0);
+		assertTrue(morpions.getWinner().equals(Owner.SECOND) && morpions.numberOfRounds() == 6 && morpions.gameOver(), "TestGetVainqueur3 : Victoire diagonale (b)");
+	}
+	
+	@Test
+	public void testGetVainqueur4() {
+		// Partie nul
+		morpions.play(0, 2);
+		morpions.play(0, 1);
+		morpions.play(1, 0);
+		morpions.play(0, 0);
+		morpions.play(1, 1);
+		morpions.play(2, 0);
+		morpions.play(2, 1);
+		morpions.play(1, 2);
+		morpions.play(2, 2);
+		assertTrue(morpions.getWinner().equals(Owner.NONE) && morpions.numberOfRounds() == 9 && morpions.gameOver(), "TestGetVainqueur4 : Match nul (b)");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
