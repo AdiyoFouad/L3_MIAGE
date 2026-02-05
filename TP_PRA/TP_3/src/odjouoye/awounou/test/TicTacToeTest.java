70c96a39e1262f5974706d1ce185e03170b5912d
AdiyoFouad <odjfouad38@gmail.com>
2026-02-05 12:04:24 +0100

    TP PRA / TP3 - TicTacToeTest

--- TP_PRA/TP_3/src/odjouoye/awounou/test/TicTacToeTest.java
+++ TP_PRA/TP_3/src/odjouoye/awounou/test/TicTacToeTest.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,13 +24,13 @@ public class TicTacToeTest {
 //		morpions = new TicTacToeV2(); //x
 //		morpions = new TicTacToeV3(); //x
 //		morpions = new TicTacToeV4(); //x
-		morpions = new TicTacToeV5();
+//		morpions = new TicTacToeV5(); //x
 //		morpions = new TicTacToeV6(); //x
 //		morpions = new TicTacToeV7(); //x
 //		morpions = new TicTacToeV8(); //x
-//		morpions = new TicTacToeV9(); 
-//		morpions = new TicTacToeV10();
-//		morpions = new TicTacToeV11();
+		morpions = new TicTacToeV9(); 
+//		morpions = new TicTacToeV10(); //x
+//		morpions = new TicTacToeV11(); //x
 //		morpions = new TicTacToeV12(); //x
 
 	}
@@ -54,8 +56,8 @@ public class TicTacToeTest {
 		assertTrue(!morpions.legalMove(0, 0), "la case (0,0) est déjà remplie");
 	}
 
-	@Test
-	public void test1() {
+	//@Test
+	public void testNumberOfRounds() {
 		morpions.play(0, 0);
 		System.out.println(morpions.numberOfRounds());
 		morpions.play(0, 1);
@@ -71,7 +73,7 @@ public class TicTacToeTest {
 	}
 	
 	@Test
-	public void testGetJoueur() {
+	public void testGetJoueur1() {
 		Owner owner = morpions.getTurn();
 
 		assertTrue(owner.equals(Owner.FIRST), "La méthode getTurn() doit retourner un Owner");
@@ -86,24 +88,38 @@ public class TicTacToeTest {
 	}
 	
 	@Test
+	public void testGetJoueur2() {
+	
+		morpions.play(0, 0);
+		morpions.play(0, 0);
+		
+		assertTrue(morpions.getTurn().equals(Owner.SECOND), "Après un play illégal on ne doit pas changer le joueur courant");		
+		
+		
+	}
+	
+	@Test
 	public void testInit() {
 		// Scénario(s) vérifiant l’état du jeu avant le premier coup :
 		// * non-fin de partie
 		// * cases accessibles
 		
 		assertTrue(morpions.getTurn().equals(Owner.FIRST), "Le joueur courant au début de la partie ne peut être autre que le joueur FIRST");
+		assertEquals(morpions.numberOfRounds(), 0, "Le nombre de coup doit être 0");
+
+		assertTrue(morpions.getWinner().equals(Owner.NONE), "Au début de la partie on ne peut pas avoir de vainqueur");
 		
 		boolean caseVide = true;
 		
-		for (int i = 0; i < 3 && caseVide; i++ ) {
-			for (int j = 0; j < 3 && caseVide; j++ ) {
-				if (!morpions.getSquare(i, j).equals(Owner.NONE)) {
+		assertTrue(!morpions.gameOver(), "La partie ne peut être terminée avant le premier coup");
+		for (int i = 0; i < TAILLE && caseVide; i++ ) {
+			for (int j = 0; j < TAILLE && caseVide; j++ ) {
+				if (!morpions.getSquare(i, j).equals(Owner.NONE) || !morpions.legalMove(i,j)) {
 					caseVide = false;
 				}
 			}
 		}
 		assertTrue(caseVide, "Aucun joueur ne devrait encore jouer au début de la partie");
-		assertTrue(!morpions.gameOver(), "La partie ne peut être terminée avant le premier coup");
 		
 	}
 	
@@ -121,10 +137,23 @@ public class TicTacToeTest {
 	@Test
 	public void testGetVainqueur0() {
 		Owner winner = morpions.getWinner();
-		assertTrue(!(winner.equals(Owner.FIRST) || winner.equals(Owner.SECOND)), "Au début de la partie on ne peut pas avoir de vainqueur");
+		assertTrue(winner.equals(Owner.NONE), "Au début de la partie on ne peut pas avoir de vainqueur");
+
+		morpions.play(0, 0);
+		winner = morpions.getWinner();
+		assertTrue(winner.equals(Owner.NONE), "Après un coup on ne peut pas avoir de vainqueur");	
+		
+		morpions.play(1, 1);
+		winner = morpions.getWinner();
+		assertTrue(winner.equals(Owner.NONE), "Après 2 coups on ne peut pas avoir de vainqueur");	
+		
+		morpions.play(2, 1);
+		winner = morpions.getWinner();
+		assertTrue(winner.equals(Owner.NONE), "Après 3 coups on ne peut pas avoir de vainqueur");	
 		
 		morpions.play(0, 1);
-		assertTrue(!(winner.equals(Owner.FIRST) || winner.equals(Owner.SECOND)), "Après un coup on ne peut pas avoir de vainqueur");	
+		winner = morpions.getWinner();
+		assertTrue(winner.equals(Owner.NONE), "Après 4 coups on ne peut pas avoir de vainqueur");	
 		
 	}
 	
@@ -249,6 +278,16 @@ public class TicTacToeTest {
 	}
 	
 	@Test
+	public void testControl0() {
+		try {
+			morpions.play(3, 3);
+		} catch(Exception e) {
+			return;
+		}
+		fail();
+	}
+	
+	@Test
 	public void testControle1() {
 		// Scénario tentant divers coups non autorisés,
 		// avec vérification systématique de legalMove()
@@ -308,15 +347,50 @@ public class TicTacToeTest {
 		
 		morpions.play(1, 0);
 		assertTrue(!morpions.legalMove(1, 0), "la case (1,0) est déjà remplie");
-		
-		
-		
-		
 
 		assertTrue(!morpions.legalMove(0, 1), "Fin de partie ! On ne peut plus remplir de case");
 		
 		
 		
+	}
+	
+	@Test
+	public void testFinPartie() {
+		// Scénarios explorant les situations de fin de partie
+		morpions.play(0, 0);
+		morpions.play(0, 1);
+		morpions.play(1, 1);
+		morpions.play(0, 2);
+		morpions.play(2, 2);
+		
+		assertTrue(morpions.getWinner().equals(Owner.FIRST));
+		
+		morpions.play(2, 0);
+
+		assertTrue(morpions.getSquare(2, 0).equals(Owner.NONE), "Après une fin de partie, aucune case ne doit être remplie");
+	}
+	
+	@Test
+	public void testFinPartie2() {
+		// Scénarios explorant les situations de fin de partie
+
+		morpions.play(0, 0);
+		morpions.play(1, 1);	
+		morpions.play(0, 1);
+		morpions.play(2, 1);
+		morpions.play(0, 2);
+		
+		assertTrue(morpions.getWinner().equals(Owner.FIRST));
+//		System.out.println(morpions.getTurn());
+		morpions.nextPlayer();
+//		System.out.println(morpions.getTurn());
+		
+		
+		morpions.play(2, 2);
+
+		System.out.println(morpions.numberOfRounds());
+
+		assertTrue(morpions.getSquare(2, 2).equals(Owner.NONE), "Après une fin de partie, aucune case ne doit être remplie");
 	}
 	
 	
